a66e3bf3c9803de4e50ee1a70cb41160471cd389
Dat Dang <dangdatcao2002@gmail.com>
2023-08-12 23:44:00 +0700

    add test

---
+++ src/test/java/controller/AccountControllerTest.java
@@ -0,0 +1,114 @@
+package controller;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.openjfx.hellofx.controller.account.AccountController;
+import org.openjfx.hellofx.models.account.Account;
+import org.openjfx.hellofx.models.account.AccountService;
+import org.openjfx.hellofx.models.card.CreditCard;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AccountControllerTest {
+
+    @Test
+    void testAccountCreate(){
+        AtomicInteger x = new AtomicInteger();
+        AtomicInteger y = new AtomicInteger();
+        AtomicInteger z = new AtomicInteger();
+
+        Account newAccount = new Account(
+                null,
+                "987654321",
+                "Dat",
+                "321",
+                "28/12",
+                1000000
+        );
+        AccountController accountController = new AccountController();
+        AccountService accountService = new AccountService();
+
+        accountService.find().forEach(dock -> x.getAndIncrement());
+        System.out.println("Before add: " + x);
+
+        accountController.createNewAccount(newAccount);
+        accountService.find().forEach(dock -> y.getAndIncrement());
+        System.out.println("After add: " + y);
+
+        accountService.findByIdAndDelete(newAccount.getId());
+        accountService.find().forEach(dock -> z.getAndIncrement());
+        System.out.println("After delete: " + z);
+        assertEquals(y.get()-1, z.get(),"delete successful");
+        assertEquals(x.get()+1, y.get(),"create successful");
+    }
+
+    @Test
+    void testMakePayment(){
+        int amount = 100;
+        AccountController accountController = new AccountController();
+        AccountService accountService = new AccountService();
+        CreditCard creditCard = new CreditCard(
+                "987654321",
+                "Dat",
+                "123",
+                "28/12"
+        );
+        Account account = accountService.findAccountByCardCode(creditCard.getCardNumber());
+        int balance_before = account.getBalance();
+        System.out.println(accountController.makePayment(creditCard, amount));
+
+        account = accountService.findAccountByCardCode(creditCard.getCardNumber());
+        int balance_after_pay = account.getBalance();
+        System.out.println(accountController.makeRefund(creditCard, amount));
+        account = accountService.findAccountByCardCode(creditCard.getCardNumber());
+        int balance_after_refund = account.getBalance();
+
+        assertEquals(balance_before, balance_after_refund, "The balance before and after pay and refund should be equal");
+        assertEquals(balance_before, balance_after_pay+amount, "The balance before pay should be greater that after pay equal to amount");
+
+        accountController.resetBalance(creditCard);
+    }
+    @Test
+    void testMakePaymentError1(){
+        AccountController accountController = new AccountController();
+        AccountService accountService = new AccountService();
+        CreditCard creditCard = new CreditCard(
+                "98754321",
+                "Dat",
+                "123",
+                "28/12"
+        );
+        int amount = 100; //cardNumber wrong
+        assertEquals(accountController.makePayment(creditCard, amount), 1, "if cardNumber wrong the method should return 1");
+        accountController.resetBalance(creditCard);
+    }
+    @Test
+    void testMakePaymentError2(){
+        AccountController accountController = new AccountController();
+        AccountService accountService = new AccountService();
+        CreditCard creditCard = new CreditCard(
+                "987654321",
+                "Dat",
+                "123",
+                "28/12"
+        );
+        int amount = 10000000; //over budget
+        assertEquals(accountController.makePayment(creditCard, amount), 2, "if account's balance is not enough the method should return 2");
+        accountController.resetBalance(creditCard);
+    }
+    @Test
+    void testMakePaymentSuccess(){
+        AccountController accountController = new AccountController();
+        AccountService accountService = new AccountService();
+        CreditCard creditCard = new CreditCard(
+                "987654321",
+                "Dat",
+                "123",
+                "28/12"
+        );
+        int amount = 100;
+        assertEquals(accountController.makePayment(creditCard, amount), 0, "the method should return 0");
+        accountController.resetBalance(creditCard);
+    }
+}
---
+++ src/test/java/controller/TransactionControllerTest.java
@@ -0,0 +1,96 @@
+package controller;
+
+import org.junit.jupiter.api.Test;
+import org.openjfx.hellofx.controller.account.AccountController;
+import org.openjfx.hellofx.controller.transaction.TransactionController;
+import org.openjfx.hellofx.models.account.Account;
+import org.openjfx.hellofx.models.account.AccountService;
+import org.openjfx.hellofx.models.transaction.Transaction;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TransactionControllerTest {
+    @Test
+    void testProgressTransaction1(){
+        String cardCode = "987654321";
+        TransactionController transactionController = new TransactionController();
+        AccountService accountService = new AccountService();
+        Transaction transaction = new Transaction(
+                null,
+                "pay",
+                "987654321",
+                "Dat",
+                "321",
+                "28/12",
+                "rent a bike",
+                100
+        );
+        Account account = accountService.findAccountByCardCode(cardCode);
+        int balance_before = account.getBalance();
+        System.out.println(transactionController.progressTransaction(transaction));
+        account = accountService.findAccountByCardCode(cardCode);
+        int balance_after = account.getBalance();
+        assertEquals(balance_before, balance_after+transaction.getAmount(), "the difference between before and after refund balance should be equal the amount");
+
+    }
+    @Test
+    void testProgressTransaction2(){
+        String cardCode = "987654321";
+        TransactionController transactionController = new TransactionController();
+        AccountService accountService = new AccountService();
+        Transaction transaction = new Transaction(
+                null,
+                "refund",
+                "987654321",
+                "Dat",
+                "321",
+                "28/12",
+                "rent a bike",
+                100
+        );
+        Account account = accountService.findAccountByCardCode(cardCode);
+        int balance_before = account.getBalance();
+        System.out.println(transactionController.progressTransaction(transaction));
+        account = accountService.findAccountByCardCode(cardCode);
+        int balance_after = account.getBalance();
+        assertEquals(balance_before, balance_after - transaction.getAmount(), "the difference between before and after refund balance should be equal the amount");
+
+    }
+    @Test
+    void testProgressTransactionError1(){
+        String cardCode = "987654321";
+        TransactionController transactionController = new TransactionController();
+        AccountService accountService = new AccountService();
+        Transaction transaction = new Transaction(
+                null,
+                "pay",
+                "98754321",
+                "Dat",
+                "321",
+                "28/12",
+                "rent a bike",
+                100
+        );
+        assertEquals(transactionController.progressTransaction(transaction), 1, "with non-exist cardCode, method should return 1");
+    }
+    @Test
+    void testProgressTransactionError2(){
+
+        String cardCode = "987654321";
+        AccountController accountController = new AccountController();
+        TransactionController transactionController = new TransactionController();
+        AccountService accountService = new AccountService();
+        Transaction transaction = new Transaction(
+                null,
+                "pay",
+                "987654321",
+                "Dat",
+                "321",
+                "28/12",
+                "rent a bike",
+                10000000
+        );
+        assertEquals(transactionController.progressTransaction(transaction), 2, "with insufficient balance, method should return 2");
+    }
+}
+
